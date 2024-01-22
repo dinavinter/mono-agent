5f1b333882d992e6f16a063b10a162ab75ce534d
dina <dina.vintr@gmail.com>
2024-01-23 00:15:12 +0200

    cachefix

--- agents/e2e/vite.config.ts
+++ agents/e2e/vite.config.ts
@@ -5,7 +5,7 @@ import tsconfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
   root: __dirname,
-  cacheDir: '../../node_modules/.vite/agents/browser',
+  cacheDir: '../../node_modules/.vite/agents/e2e',
 
   plugins: [nxViteTsPaths(), tsconfigPaths()],
 
