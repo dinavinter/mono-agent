05d6028e180a80fd16476447619083ee1568af99
dina <dina.vintr@gmail.com>
2024-01-22 23:17:01 +0200

    vite config

--- apps/vite/vite.config.ts
+++ apps/vite/vite.config.ts
@@ -1,16 +1,22 @@
 import { unstable_vitePlugin as remix } from "@remix-run/dev";
-import { defineConfig } from "vite";
+import {defineConfig, UserConfig} from 'vite';
 import tsconfigPaths from "vite-tsconfig-paths";
 import {nxViteTsPaths} from '@nx/vite/plugins/nx-tsconfig-paths.plugin';
 import { installGlobals } from "@remix-run/node";
 // import { cjsInterop } from "vite-plugin-cjs-interop";
+import { visualizer } from "rollup-plugin-visualizer";
+import {createWatchPaths} from '@nx/remix';
 
 installGlobals();
 
-async function getConfig() {
+async function getConfig():Promise<UserConfig> {
+  // const process =await import('process');
+  //const cjsInterop =await import('vite-plugin-cjs-interop');
   const tailwindcss =await import('tailwindcss')
   const autoprefixer =await import('autoprefixer')
-
+  // const watchPath = await createWatchPaths(__dirname)
+  // console.log(watchPath);
+  // console.log(process.cwd())
   return {
 
     cacheDir: '../../node_modules/.vite/apps/vite',
@@ -37,16 +43,14 @@ async function getConfig() {
     //    needsInterop: ['@mono-agent/browser']
     // },
     // ssr:{
-    //   external: ['@mono-agent/browser'],
+    //   external: ['@mono-agent/tester'],
     //   target:'node'
     // },
 
     plugins: [tsconfigPaths(),
       // cjsInterop({
-      // // List of CJS dependencies that require interop
       // dependencies: [
-      //   '@mono-agent/browser',
-      //   '@mono-agent/browser/*',
+      //   '@mono-agent/tester'
       //
       // ]}),
       remix({
@@ -59,9 +63,19 @@ async function getConfig() {
         publicPath: '/',
         serverBuildDirectory: 'build/server',
 
+        // watchPaths: () => require('@nx/remix').createWatchPaths(__dirname),
+        // appDirectory: `app`,
+        //
+        // assetsBuildDirectory: `${process.cwd()}/dist/apps/vite/client`,
+        // publicPath: `${process.cwd()}/dist/apps/vite/public`,
+        // serverBuildDirectory: `${process.cwd()}/dist/apps/vite/server`,
+        // unstable_ssr:true
         // browserNodeBuiltinsPolyfill: { modules: { path: true, fs:true,  "fs/promises": true, child_process: true , inspector: true ,readline: true, constants: true, http: true,  https: true, crypto: true   } },
 
-      }), nxViteTsPaths()],
+      }), 
+      nxViteTsPaths(),
+      visualizer({ emitFile: true }),
+    ],
   };
 }
 
